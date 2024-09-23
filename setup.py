c61b6d9372bdbb5368e10d321d54b1c3043e3184
ricklisz <yli3445@gatech.edu>
2024-09-23 00:58:12 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        name="nnunetv2",  # Replace with your project name
+        version="0.1",
+        packages=setuptools.find_packages(exclude=['figs']),  # Exclude the figs folder
+        install_requires=[
+            # List your dependencies here, for example:
+            # "numpy", "torch", etc.
+        ],
+    )
+
